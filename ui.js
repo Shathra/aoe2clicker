1d199d67e911580ef12b53d4ff10cf7b9c4eb318
root <aberkay37@gmail.com>
2016-03-05 04:25:54 +0200

    sh script

--- ui.js
+++ ui.js
@@ -1,15 +1,18 @@
 $(document).ready(function(){
 	$(document).on('mousedown', ".click_resource", function(){
-		$(this).children().css("height", "170px").css("width", "170px");
-		$(this).children().css("margin", "20px 8px");
-
+		$(this).find("img").css("height", "170px").css("width", "170px");
+		$(this).find("img").css("margin", "20px 8px");
 	});
+
 	$(document).on("mouseup", ".click_resource", function(){
-		$(this).children().css("height", "200px").css("width", "200px");
-		$(this).children().css("margin","auto");
+		$(this).find("img").css("height", "200px").css("width", "200px");
+		$(this).find("img").css("margin","auto");
 	});
 
-
+	$(document).on("mouseover", ".click_resource", function(){
+		$(this).find("img").css("height", "200px").css("width", "200px");
+		$(this).find("img").css("margin","auto");
+	});
 
 
 
